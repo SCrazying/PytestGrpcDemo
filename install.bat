ffc9384d2af724f8c5ead814310813bc598d0d0d
SCrazying <locojyw@outlook.com>
2023-04-02 19:18:22 +0800

    更新 install

--- install.bat
+++ install.bat
@@ -1,10 +1,1 @@
-:: install pytest
-pip install pytest
-:: install allure
-pip install allure-pytest
-:: install proto/gprc tool
-pip install grpcio
-pip install grpcio.tools
-:: pytest-grpc framework
-pip install pytest-grpc
-
+pip install -r requirements.txt
